1265389dec4f0972f8a76f2e32a81ed0f7a97651
sandeshkadam810 <142660928+sandeshkadam810@users.noreply.github.com>
2023-08-29 01:07:51 +0530

    latest

--- script.js
+++ script.js
@@ -5,15 +5,6 @@ openMenu.addEventListener('click', () => {
     sidebar.classList.toggle('sidebar-active');
 });
 
-
-// script.js
-
-// Function to handle the login button click event
-function handleLoginButtonClick() {
-    // Redirect the user to the index.html page
-    window.location.href = 'index.html';
-}
-
 // Add an event listener to the login button
 document.addEventListener('DOMContentLoaded', function () {
     const loginButton = document.querySelector('.navbar-links li:last-child a');
@@ -22,14 +13,25 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-const faqShowButtons = document.querySelectorAll(".faq-show");
+    document.addEventListener("DOMContentLoaded", function() {
+        var faqContainers = document.querySelectorAll(".faq");
 
-        faqShowButtons.forEach(button => {
-            button.addEventListener("click", () => {
-                const answer = button.nextElementSibling;
-                answer.classList.toggle("hidden");
+        faqContainers.forEach(function(container) {
+            var question = container.querySelector(".faq-question");
+            var answer = container.querySelector(".faq-answer");
+            var showButton = container.querySelector(".faq-show");
+
+            showButton.addEventListener("click", function() {
+                if (answer.style.display === "none" || answer.style.display === "") {
+                    answer.style.display = "block";
+                    showButton.textContent = "Hide";
+                } else {
+                    answer.style.display = "none";
+                    showButton.textContent = "Show";
+                }
             });
         });
+    });
 
 
 
